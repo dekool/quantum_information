d7bf7fe3a97b1e57cce15535e708a4e7bb2dd17f
dekel <34662967+dekool@users.noreply.github.com>
2020-01-12 20:45:48 +0200

    rearrange the code

--- MPS.py
+++ MPS.py
@@ -1,75 +1,86 @@
 import numpy as np
 from numpy import tensordot
 
-def concate_tensors(tensor_a, tensor_b, leg_to_sum_a, leg_to_sum_b):
-    return tensordot(tensor_a, tensor_b, axes=([leg_to_sum_a], [leg_to_sum_b]))
-
-
-def sum_left_side_with_next_A_old(left_side, A, with_B=False):
-    # start computing from left to right (all the tensors hold real values so conjugate is unnessecery
-    temp = concate_tensors(left_side, A, 0, 1)
-    print(temp.size)
-    temp = concate_tensors(temp, A, 0, 1)
-    print(temp.size)
-    if with_B:
-        temp = concate_tensors(B, temp, 0, 0)
-    print(concate_tensors(temp, A, 0, 2).size)
-    return concate_tensors(temp, A, 0, 2)
-
 def sum_left_side_with_next_A(left_side, A):
-    # start computing from left to right (all the tensors hold real values so conjugate is unnessecery
+    """
+    join both A matrices and than contract them with the left side of the state
+    :return: new tensor representing the left side of the computation
+    """
+    # all the tensors hold real values so conjugate is unnecessary
     new_A = tensordot(A, A, axes=([0], [0]))
     return tensordot(left_side, new_A, axes=([0, 1], [0, 2]))
 
 def sum_left_side_with_B_and_next_A(left_side, A, B):
-    # start computing from left to right (all the tensors hold real values so conjugate is unnessecery
+    """
+    first join the upper A with the B matrix, and than contract the result with the bottom A
+    and than contract the result with the left side of the state
+    :return: new tensor representing the left side of the computation
+    """
+    # all the tensors hold real values so conjugate is unnecessary
     temp_a = tensordot(B, A, axes=([0], [0]))
     new_A = tensordot(temp_a, A, axes=([0], [0]))
     return tensordot(left_side, new_A, axes=([0, 1], [0, 2]))
 
-N = 10
+def initialize_matrices():
+    D = 2
+    # Initialize the A,L,R tensors using random values just for fixing their sizes easily
+    A = np.random.normal(size=[D, D, D])
+    L = np.random.normal(size=[D, D])
+    R = np.random.normal(size=[D, D])
 
-D = 2
-# Initialize the A,B tensors using random values
-A = np.random.normal(size=[D, D, D])
-L = np.random.normal(size=[D, D])
-R = np.random.normal(size=[D, D])
+    B = [[1, 0.5], [0.5, -2]]
+    # build the tensors
+    for j in range(2):
+        for a in range(2):
+            for b in range(2):
+                A[j][a][b] = np.sin(2 * j + a) - 0.7 * b
+    for j in range(2):
+        for a in range(2):
+            L[j][a] = np.cos(j * a)
+            # for R: a==b
+            R[j][a] = j + a
+    return A, B, L, R
 
-B = [[1, 0.5], [0.5, -2]]
-# build the tensors
-for j in range(2):
-    for a in range(2):
-        for b in range(2):
-            A[j][a][b] = np.sin(2*j + a) - 0.7*b
-for j in range(2):
-    for a in range(2):
-        L[j][a] = np.cos(j*a)
-        # for R: a==b
-        R[j][a] = j + a
-
-answer = []
-for N in range(10, 110, 10):
+def calculate_psi_B_psi(N):
+    """
+    calculates <psi|B|psi> from left side to the right. holds maximum 6 "open legs" at a given time
+    :return: number - the result of <psi|B|psi>
+    """
+    n = (2 * N) - 1
     # temp: hold a Tensor which keeps the computation
-    temp = concate_tensors(L, L, 0, 0)
-    n = (2*N)-1
-    # first, calculate the <psi|B|psi>
-    for i in range(1, n-1):
+    temp = tensordot(L, L, axes=([0], [0]))
+
+    for i in range(1, n - 1):
         if i == N:
             temp = sum_left_side_with_B_and_next_A(temp, A, B)
         else:
             temp = sum_left_side_with_next_A(temp, A)
     # sum with the Rs
-    new_R = concate_tensors(R, R, 0, 0)
-    result = tensordot(temp, new_R, axes=([0, 1], [0, 1]))
+    new_R = tensordot(R, R, axes=([0], [0]))
+    return tensordot(temp, new_R, axes=([0, 1], [0, 1]))
 
-    # calculate norm
-    temp = concate_tensors(L, L, 0, 0)
-    for i in range(1, n-1):
+def calculate_norm(N):
+    """
+    calculate the norm of the state. doing than using tensors - from left to right.
+    holds maximum 6 "open legs" at a given time
+    :return: the norm of psi
+    """
+    n = (2 * N) - 1
+    # temp: hold a Tensor which keeps the computation
+    temp = tensordot(L, L, axes=([0], [0]))
+
+    for i in range(1, n - 1):
         temp = sum_left_side_with_next_A(temp, A)
     # sum with the Rs
-    new_R = concate_tensors(R, R, 0, 0)
+    new_R = tensordot(R, R, axes=([0], [0]))
     temp_num = tensordot(temp, new_R, axes=([0, 1], [0, 1]))
-    norm = np.abs(temp_num)
+    return np.abs(temp_num)
 
-    answer.append(result/norm)
+A,B,L,R = initialize_matrices()
+answer = {}
+for N in range(10, 110, 10):
+    result = calculate_psi_B_psi(N)
+    norm = calculate_norm(N)
+
+    answer[N] = result/norm
 print(answer)
